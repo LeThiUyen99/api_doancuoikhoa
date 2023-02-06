1a1a471bc9d0ab8ece3afe2cfa6c898e5b46351c
LeThiUyen99 <uyenlt@vgsholding.com>
2023-02-06 11:26:54 +0700

    role

--- src/router/admin.router.js
+++ src/router/admin.router.js
@@ -1,109 +1,129 @@
-let router = require('./extensions').Router()
-let models = router.models
-let redispool = require('../utils/redispool')
-let db = require('../utils/mysqlpool')
+let router = require("./extensions").Router();
+let models = router.models;
+let redispool = require("../utils/redispool");
+let db = require("../utils/mysqlpool");
 // let untils = require('../utils/')
-const {ThrowReturn} = require('./extensions')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const {getTotalPage} = require('../lib/formatData')
-const {all} = require('bluebird')
-const {isEmpty} = require('../lib/validate')
+const { ThrowReturn } = require("./extensions");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { getTotalPage } = require("../lib/formatData");
+const { all } = require("bluebird");
+const { isEmpty } = require("../lib/validate");
 
 const encodePassword = (password) =>
   new Promise((resolve, reject) =>
     bcrypt.hash(password, 8, function (err, hash) {
-      if (err) reject(err)
-      resolve(hash)
+      if (err) reject(err);
+      resolve(hash);
     })
-  )
+  );
 
 const comparePassword = (password, hashedPassword) =>
   new Promise((resolve) =>
     bcrypt.compare(password, hashedPassword, (err, result) => {
-      console.log(result, password, hashedPassword)
-      if (err) resolve(false)
-      if (result) resolve(true)
-      else resolve(false)
+      console.log(result, password, hashedPassword);
+      if (err) resolve(false);
+      if (result) resolve(true);
+      else resolve(false);
     })
-  )
+  );
 
 const generateToken = (data) =>
   jwt.sign(data, process.env.TOKEN_PRIVATE_KEY, {
-    expiresIn: 60 * 60 * 24
-  })
+    expiresIn: 60 * 60 * 24,
+  });
 
 async function register(req, res) {
-  let {name, username, password, phone, email} = req.body
-  const newPassword = await encodePassword(password)
+  let { name, username, password, phone, email } = req.body;
+  const newPassword = await encodePassword(password);
   const createAdmin = await models.AdminCm.findOrCreate({
-    where: {user_name: username},
-    defaults: {name, user_name: username, password: newPassword, phone, email}
-  })
-  if (!createAdmin[1]) throw new ThrowReturn('Admin existed')
-  res.sendData({data: createAdmin})
+    where: { user_name: username },
+    defaults: {
+      name,
+      user_name: username,
+      password: newPassword,
+      phone,
+      email,
+    },
+  });
+  if (!createAdmin[1]) throw new ThrowReturn("Admin existed");
+  res.sendData({ data: createAdmin });
 }
 
 async function login(req, res) {
-  let {username, password} = Object.assign({}, req.query, req.body)
+  let { username, password } = Object.assign({}, req.query, req.body);
 
-  let account = await models.AdminCm.findOne({where: {user_name: username}})
-  if (!account) throw new ThrowReturn('Admin not exist')
-  console.log(account)
+  let account = await models.AdminCm.findOne({
+    where: { user_name: username },
+  });
+  if (!account) throw new ThrowReturn("Admin not exist");
+  console.log(account);
   if (account.is_active === 0)
-    throw new ThrowReturn('Tài khoản chưa được kích hoạt. Vui lòng liên hệ với admin!')
-  const isValid = await comparePassword(password, account.password)
-  if (!isValid) throw new ThrowReturn('Password incorrect')
+    throw new ThrowReturn(
+      "Tài khoản chưa được kích hoạt. Vui lòng liên hệ với admin!"
+    );
+  const isValid = await comparePassword(password, account.password);
+  if (!isValid) throw new ThrowReturn("Password incorrect");
 
-  const token = generateToken({id: account.id})
-  delete account.dataValues.password
+  const token = generateToken({ id: account.id });
+  delete account.dataValues.password;
 
-  res.sendData({token, account})
+  res.sendData({ token, account });
 }
 
 async function list(req, res) {
-  let {limit, page} = req.query
-  if (isEmpty(limit)) limit = 10
-  if (isEmpty(page)) page = 1
+  let { limit, page } = req.query;
+  if (isEmpty(limit)) limit = 10;
+  if (isEmpty(page)) page = 1;
   const admins = await models.AdminCm.findAll({
     offset: parseInt(page - 1),
-    limit: parseInt(limit)
-  })
-  const totalPgae = getTotalPage(all.length, limit)
+    limit: parseInt(limit),
+  });
+  const totalPgae = getTotalPage(all.length, limit);
   // throw new Error('nothing')
-  res.sendData({admins, totalPgae})
+  res.sendData({ admins, totalPgae });
 }
 
 async function update(req, res) {
-  const data = req.body
-  await models.AdminCm.update(data, {where: {id: req.body.id}})
+  const data = req.body;
+  await models.AdminCm.update(data, { where: { id: req.body.id } });
 
-  return res.sendData(null, 'Update success!')
+  return res.sendData(null, "Update success!");
 }
 
 async function remove(req, res) {
-  let {id} = req.params
-  await models.AdminCm.destroy({where: {id: id}})
-  return res.sendData(null, 'Remove success!')
+  let { id } = req.params;
+  await models.AdminCm.destroy({ where: { id: id } });
+  return res.sendData(null, "Remove success!");
 }
 
 async function menus(req, res) {
-  await models.Menu.findAll({})
+  await models.Menu.findAll({});
 
-  return res.sendData({data})
+  return res.sendData({ data });
 }
 
 async function update_active(req, res) {
-  await models.AdminCm.update({is_active: req.body.is_active}, {where: {id: req.body.id}})
+  await models.AdminCm.update(
+    { is_active: req.body.is_active },
+    { where: { id: req.body.id } }
+  );
 
-  return res.sendData(null, 'Kich hoat thanh cong')
+  return res.sendData(null, "Kich hoat thanh cong");
 }
 
-router.postS('/register', register, false)
-router.postS('/login', login, false)
-router.postS('/update', update, false)
-router.getS('/list', list, false)
-router.getS('/delete/:id', remove, false)
-router.getS('/menus', menus, false)
-router.postS('/update_active', update_active, false)
-module.exports = router
+async function detail(req, res) {
+  // const data = await models.AdminCm.findOne({ where: { id: req.query.id } });
+  const data = req.currentAdmin;
+  return res.sendData({ data });
+}
+
+router.postS("/register", register, false);
+router.postS("/login", login, false);
+router.postS("/update", update, false);
+router.getS("/list", list, false);
+router.getS("/detail", detail, true);
+router.getS("/delete/:id", remove, false);
+router.getS("/menus", menus, false);
+router.postS("/update_active", update_active, false);
+module.exports = router;
--- src/router/book_tour.router.js
+++ src/router/book_tour.router.js
@@ -1,67 +1,71 @@
-let router = require('./extensions').Router()
-let models = router.models
-let redispool = require('../utils/redispool')
-let db = require('../utils/mysqlpool')
-const {ThrowReturn} = require('./extensions')
-const {getTotalPage} = require('../lib/formatData')
-const {all} = require('bluebird')
-const {isEmpty} = require('../lib/validate')
-const {Op} = require('sequelize')
+let router = require("./extensions").Router();
+let models = router.models;
+let redispool = require("../utils/redispool");
+let db = require("../utils/mysqlpool");
+const { ThrowReturn } = require("./extensions");
+const { getTotalPage } = require("../lib/formatData");
+const { all } = require("bluebird");
+const { isEmpty } = require("../lib/validate");
+const { Op } = require("sequelize");
 
 async function list(req, res) {
-  let condition = {}
+  let condition = {};
 
   switch (req.query.filter) {
-    case '1':
-      condition.id = req.query.q
-      break
-    case '2':
-      condition.name = {[Op.substring]: req.query.q}
-      break
+    case "1":
+      condition.id = req.query.q;
+      break;
+    case "2":
+      condition.name = { [Op.substring]: req.query.q };
+      break;
   }
-  let {limit, page} = req.query
-  if (isEmpty(limit)) limit = 10
-  if (isEmpty(page)) page = 1
+  let { limit, page } = req.query;
+  if (isEmpty(limit)) limit = 10;
+  if (isEmpty(page)) page = 1;
   const list = await models.TourBooked.findAll({
     where: condition,
-    include: [{as: 'tour', model: models.Tour}],
+    include: [{ as: "tour", model: models.Tour }],
     offset: parseInt(page - 1),
-    limit: parseInt(limit)
-  })
+    limit: parseInt(limit),
+  });
 
-  const totalPage = getTotalPage(all.length, limit)
+  const totalPage = getTotalPage(all.length, limit);
   // throw new Error('nothing')
-  return res.sendData({data: list, totalPage})
+  return res.sendData({ data: list, totalPage });
 }
 
 async function create(req, res) {
-  let data = req.body
-  await models.TourBooked.create(data)
-  await models.Tour.update({sold_number: +data.dataValues.sold_number + 1}, {where: {id: req.query.tour_id}})
-  res.sendData(null, 'Update success!')
+  const data = await models.Tour.findByPk(req.body.tour_id);
+  console.log(data);
+  await models.TourBooked.create(req.body);
+  await models.Tour.update(
+    { sold_number: +data.dataValues.sold_number + 1 },
+    { where: { id: req.body.tour_id } }
+  );
+  res.sendData(null, "Bạn đã đặt tour thành công");
 }
 
 async function update(req, res) {
-  const data = req.body
-  await models.TourBooked.update(data, {where: {id: req.body.id}})
+  const data = req.body;
+  await models.TourBooked.update(data, { where: { id: req.body.id } });
 
-  return res.sendData(null, 'Update success!')
+  return res.sendData(null, "Update success!");
 }
 
 async function remove(req, res) {
-  let {id} = req.params
-  await models.TourBooked.destroy({where: {id: id}})
-  return res.sendData(null, 'Remove success!')
+  let { id } = req.params;
+  await models.TourBooked.destroy({ where: { id: id } });
+  return res.sendData(null, "Remove success!");
 }
 
 async function update_active(req, res) {
-  await models.TourBooked.update({active: 1}, {where: {id: req.body.id}})
-  return res.sendData(null, 'Xác nhận thành công!')
+  await models.TourBooked.update({ active: 1 }, { where: { id: req.body.id } });
+  return res.sendData(null, "Xác nhận thành công!");
 }
 
-router.getS('/list', list, false)
-router.postS('/create', create, false)
-router.postS('/update', update, false)
-router.getS('/delete/:id', remove, false)
-router.postS('/update_active', update_active, false)
-module.exports = router
+router.getS("/list", list, false);
+router.postS("/create", create, false);
+router.postS("/update", update, false);
+router.getS("/delete/:id", remove, false);
+router.postS("/update_active", update_active, false);
+module.exports = router;
