6a8684fbd8dcbcfcbd6a771e45a2484a8ff0e821
kiendt12640 <83637597+LeThiUyen99@users.noreply.github.com>
2023-02-17 15:23:57 +0700

    .

--- src/router/message.router.js
+++ src/router/message.router.js
@@ -3,14 +3,26 @@ let models = router.models
 
 
 async function list(req, res) {
-  const {room} = req.query
-    const list = await models.Message.findAll({ where: {room}})
-    return res.sendData({list})
-  }
+  const { room } = req.query
+  const page = +req.query.page || 1
+  const limit = +req.query.limit || 10
 
-  async function rooms(req, res) {
-    const rooms = await models.Message.findAll({
-      include: [{model: models.User, as: "user"}],
+  const { rows, count } = await models.Message.findAndCountAll({
+    where: { room },
+    offset: (page - 1) * limit,
+    limit: limit,
+    order: [["created_at", "ASC"]]
+  })
+
+  let isLast = false
+  if ((page * limit) >= count) isLast = true
+
+  return res.sendData({ list: rows, isLast })
+}
+
+async function rooms(req, res) {
+  const rooms = await models.Message.findAll({
+    include: [{ model: models.User, as: "user"}],
     })
 
     let unique = []
